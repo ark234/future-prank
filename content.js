c3a7142c1c633e332ebfc0b8cfddf30d436b3bc0
ark234 <ark234@gmail.com>
2018-08-02 00:03:00 -0700

    adding jquery, changing content to use jquery, adding jquery to manifest

--- content.js
+++ content.js
@@ -1,12 +1,16 @@
 const priceChanger = () => {
-	let prices = document.getElementsByClassName('sx-price-whole');
-	let fractions = document.getElementsByClassName('sx-price-fractional');
+	// let prices = document.getElementsByClassName('sx-price-whole');
+	// let fractions = document.getElementsByClassName('sx-price-fractional');
+	let $prices = $('.sx-price-whole');
+	let $fractions = $('.sx-price-fractional');
+	console.log('prices', $prices[0]);
+	console.log('fractions', $fractions[0]);
 
-	for (let i = 0; i < prices.length; i++) {
+	for (let i = 0; i < $prices.length; i++) {
 		let randDollar = Math.floor(Math.random() * 2);
 		let randCent = Math.floor(Math.random() * 89) + 10;
-		prices[i].innerHTML = randDollar;
-		fractions[i].innerHTML = randCent;
+		$($prices[i]).text(randDollar);
+		$($fractions[i]).text(randCent);
 	}
 };
 
