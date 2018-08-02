011ec37dd588196d98f56e3faf7085b8db64d9fb
ark234 <ark234@gmail.com>
2018-08-01 22:45:11 -0700

    adding pricechanger to content

--- content.js
+++ content.js
@@ -1,1 +1,17 @@
-// content.js
+const priceChanger = () => {
+	let prices = document.getElementsByClassName('sx-price-whole');
+	let fractions = document.getElementsByClassName('sx-price-fractional');
+
+	for (let i = 0; i < prices.length; i++) {
+		let randDollar = Math.floor(Math.random() * 2);
+		let randCent = Math.floor(Math.random() * 89) + 10;
+		prices[i].innerHTML = randDollar;
+		fractions[i].innerHTML = randCent;
+	}
+};
+
+const init = () => {
+	priceChanger();
+};
+
+init();
