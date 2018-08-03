249926eb7318d0a1bdd3cfc2f5ac0a6613a1d864
Dylan Bergstrom <contact.dylanbergstrom@gmail.com>
2018-08-02 18:06:23 -0700

    added new sites

--- content.js
+++ content.js
@@ -1,29 +1,45 @@
 const priceChanger = () => {
-	// let prices = document.getElementsByClassName('sx-price-whole');
-	// let fractions = document.getElementsByClassName('sx-price-fractional');
+
+	//These variables target specific classes so that we can manipulate later
 	let $prices = $('.sx-price-whole');
 	let $fractions = $('.sx-price-fractional');
 	let $googleFront = $('.O8U6h');
-	let $googleDeep = $('.e10twf T4OwTb');
+	//let $googleDeep2 = $('.qptdjc')
+	let $googleDeep = $('.e10twf');
 	let $amazon = $('#priceblock_ourprice');
 	let $ebay = $('.s-item__price');
 
-	let dollarBank = [$prices, $fractions, $googleFront, $googleDeep, $ebay];
-	let centBank = [$fractions];
 
+	// our list of sites to cycle through
+	let dollarBank = [/*$prices, $fractions,*/ $googleFront, $googleDeep, $ebay];
+	//let centBank = [$fractions];
+
+	// edge case sites that need different formatting 
 	$amazon.text(genRandDollar());
+	$prices.text(genRandDollar());
+	$fractions.text(genRandCent());
 
+
+	// cycles through site classes and applies new prices for DOLLAR values
 	dollarBank.forEach(el => {
+		console.log(el);
 		$(el).each((index, value) => {
-			$(value).text(genRandDollar());
+			$(value).text(genRandDollar2());
 		});
 	});
 
+	//cycles thru site classes and applies new prices for CENT values
 	centBank.forEach(el => {
 		$(el).each((index, value) => {
 			$(value).text(genRandCent());
 		});
 	});
+};
+
+// Generate random numbers
+
+const genRandDollar2 = () => {
+	return `$ ${Math.floor(Math.random() * 2)}.${genRandCent()}`;
 };
 
 const genRandDollar = () => {
